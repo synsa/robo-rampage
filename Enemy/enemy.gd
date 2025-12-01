3c3a86d34a9d0dd506c347b6f2965d30c8b9dff5
Steve Lang <itsme@stevelang.dev>
2025-11-30 23:44:47 -0700

    enemy has an attack_range

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+@export var attack_range := 1.5
+
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 
 var player
@@ -29,7 +31,12 @@ func _physics_process(delta: float) -> void:
 	if distance <= aggro_range:
 		provoked = true
 
+	if provoked:
+		if distance <= attack_range:
+			print("enemy attack!")
+
 	if direction:
+		look_at_target(direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -37,3 +44,8 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
 	move_and_slide()
+	
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	look_at(global_position + adjusted_direction, Vector3.UP, true)
