091bb817833d0d06079c984281dfd23ecc2b97b3
Steve Lang <itsme@stevelang.dev>
2025-12-27 10:46:22 -0700

    Cycle through weapons with mouse wheel

--- Player/weapon_handler.gd
+++ Player/weapon_handler.gd
@@ -7,10 +7,14 @@ func _ready() -> void:
 	equip(weapon_1)
 		
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_just_pressed("weapon_1"):
+	if event.is_action_pressed("weapon_1"):
 		equip(weapon_1) 
-	if event.is_action_just_pressed("weapon_2"):
-		equip(weapon_2) 
+	if event.is_action_pressed("weapon_2"):
+		equip(weapon_2)
+	if event.is_action_pressed("next_weapon"):
+		next_weapon()
+	if event.is_action_pressed("prev_weapon"):
+		next_weapon()
 		
 func equip(active_weapon: Node3D) -> void:
 	for child in get_children():
@@ -20,3 +24,19 @@ func equip(active_weapon: Node3D) -> void:
 		else:
 			child.visible = false
 			child.set_process(false)
+
+func next_weapon() -> void:
+	var index = get_current_index()
+	index = wrapi(index + 1, 0, get_child_count())
+	equip(get_child(index))
+
+func prev_weapon() -> void:
+	var index = get_current_index()
+	index = wrapi(index - 1, 0, get_child_count())
+	equip(get_child(index))
+
+func get_current_index() -> int:
+	for index in get_child_count():
+		if get_child(index).visible:
+			return index
+	return 0
